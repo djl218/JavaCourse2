88ccb52d7209061588bf88a29338d9a75d671629
Daniel Leskosky <dleskosky@gmail.com>
2020-09-13 21:50:59 -0400

    Commit for exercise 8.3

---
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class LiquidContainers {
+
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 0;
+        Scanner scan = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
+
+            String input = scan.nextLine();
+            System.out.println("");
+            if (input.equals("quit")) {
+                break;
+            }
+                
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (command.equals("add") && amount > 0) {
+                first = first + amount;
+                if (first > 100) {
+                    first = 100;
+                }
+            } else if (command.equals("move") && amount > 0) {
+                if (amount > first) {
+                    second = second + first;
+                } else {
+                    second = second + amount;
+                }
+                
+                first = first - amount;
+                
+                if (first < 0) {
+                    first = 0;
+                }
+                if (second > 100) {
+                    second = 100;
+                }
+            } else if (command.equals("remove") && amount > 0) {
+                second = second - amount;
+                if (second < 0) {
+                    second = 0;
+                }
+            }
+
+        }
+    }
+
+}
