3a71008141702e117fbd76d45211188e50526198
Daniel Leskosky <dleskosky@gmail.com>
2020-09-26 17:52:10 -0400

    Commit for exercise 9.12

---
+++ part09-Part09_12.Animals/src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat extends Animal implements NoiseCapable {
+    
+    public Cat() {
+        super("Cat");
+    }
+    
+    public Cat(String name) {
+        super(name);
+    }
+    
+    public void purr() {
+        System.out.println(getName() + " purrs");
+    }
+    
+    @Override
+    public void makeNoise() {
+        this.purr();
+    }
+}
