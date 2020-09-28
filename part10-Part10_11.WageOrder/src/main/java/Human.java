c61c78bece042bb40720d5dd8a397ed4ea868394
Daniel Leskosky <dleskosky@gmail.com>
2020-09-28 19:58:23 -0400

    Commit for exercise 10.11

---
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -0,0 +1,35 @@
+public class Human implements Comparable<Human> {
+
+    private int wage;
+    private String name;
+
+    public Human(String name, int wage) {
+        this.name = name;
+        this.wage = wage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWage() {
+        return wage;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + " " + wage;
+    }
+    
+    @Override
+    public int compareTo(Human human) {
+        if (this.wage == human.getWage()) {
+            return 0;
+        } else if (this.wage < human.getWage()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
