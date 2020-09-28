52e71cbb066c6f6bf09550442de7cbe58751fcea
Daniel Leskosky <dleskosky@gmail.com>
2020-09-28 17:46:01 -0400

    Commit for exercise 10.8

---
+++ part10-Part10_08.Weighting/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Just used tests to make sure it worked.
+    }
+
+}
---
+++ part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Suitcase {
+
+    private int maximumWeight;
+    private ArrayList<Item> Items;
+
+    public Suitcase(int maximumWeight) {
+        this.Items = new ArrayList<>();
+        this.maximumWeight = maximumWeight;
+    }
+
+    public void addItem(Item Item) {
+        if (this.totalWeight() + Item.getWeight() > this.maximumWeight) {
+            return;
+        }
+
+        this.Items.add(Item);
+    }
+
+    public int totalWeight() {
+        return Items.stream()
+            .reduce(0, (item1, item2) -> item1 + item2.getWeight(), Integer::sum);
+    }
+
+    public void printItems() {
+        Items.stream()
+            .forEach(item -> System.out.println(item));
+    }
+
+    public Item heaviestItem() {
+        if (this.Items.isEmpty()) {
+            return null;
+        }
+
+        // Tutustumme järjestämiseen hieman myöhemmin kurssilla
+        return this.Items.stream().max((t1, t2) -> t1.getWeight() - t2.getWeight()).get();
+    }
+
+    @Override
+    public String toString() {
+        if (this.Items.isEmpty()) {
+            return "no items (0 kg)";
+        }
+
+        if (this.Items.size() == 1) {
+            return "1 item (" + this.totalWeight() + " kg)";
+        }
+
+        return this.Items.size() + " items (" + this.totalWeight() + " kg)";
+    }
+}
