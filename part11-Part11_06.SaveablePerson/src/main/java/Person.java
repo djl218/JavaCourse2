22d3f4dbe454f05b8aa515474ee864a8dde08e4d
Daniel Leskosky <dleskosky@gmail.com>
2020-10-18 22:21:15 -0400

    Commit for exercise 11.6

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,16 @@
+public class Person implements Saveable {
+    private String name;
+    private String address;
+    
+    public void save() {
+        
+    }
+    
+    public void delete() {
+        
+    }
+    
+    public void load(String address) {
+        
+    }
+}
