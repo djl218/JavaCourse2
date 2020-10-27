015409945f9bfe668ed54618ad5dcd30dad37d42
Daniel Leskosky <dleskosky@gmail.com>
2020-10-27 12:16:53 -0400

    Commit for exercise 13.3

---
+++ part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -0,0 +1,31 @@
+package buttonandtextfield;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ButtonAndTextFieldApplication extends Application {
+
+    @Override
+    public void start(Stage window) {
+        Button buttonComponent = new Button("This is a button");
+        TextField textFieldComponent = new TextField("Text field");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(buttonComponent);
+        componentGroup.getChildren().add(textFieldComponent);
+
+        Scene view = new Scene(componentGroup);
+
+        window.setScene(view);
+        window.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(ButtonAndTextFieldApplication.class);
+    }
+
+}
